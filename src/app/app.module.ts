359e21771f4cd0080c30eb125c8e059872f9da35
Kondarattsev Denis Sergeevich <d.kondarattsev@tinkoff.ru>
2019-05-17 18:37:11 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import 'hammerjs';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { AppMaterialModule } from './app.material.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppMaterialModule,
+    BrowserAnimationsModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
